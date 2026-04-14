07c3516df6fc0d26b6ffdf76e974e14b4f0b8ee4
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2026-04-14 19:38:22 +0100

    dafetiteFooter.js:

---
+++ totalmanschool_frontend/src/hooks/dafetiteFooter/dafetiteFooter.js
@@ -0,0 +1,32 @@
+import { DateHook } from "./dateHook"
+import './dafetiteFooter.css'
+import dafetite from './dafelogoWhiteTransparent.png'
+
+function DafetiteFooter() {
+	const year = DateHook().todayYear;
+	return (
+		<>
+			<span className="DafetiteFooter span-dafetite">
+				<sup>&copy;</sup>Copyright {year} | Developed by
+				<a
+				target="_blank"
+				rel="noopener noreferrer"
+				href="https://dafetite.pages.dev/">
+					<img
+					src={dafetite}
+					alt="dafetite ogaga"
+					className="dafetite" />
+				</a>
+			</span>
+		</>
+	)
+}
+// const styles = {
+// 	imgStyle: {
+// 		width: '50px',
+// 		height: '50px',
+// 		borderRadius: '50%',
+// 		marginLeft: '5px',
+// 	}
+// }
+export { DafetiteFooter };

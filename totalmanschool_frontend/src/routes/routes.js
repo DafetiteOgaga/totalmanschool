d80f3847b1435e32986e70d08944f813027ebfc3
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2026-04-14 19:38:33 +0100

    routes.js:

---
+++ totalmanschool_frontend/src/routes/routes.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { ProtectedRoute, PublicRoute } from './ProtectedRoute';
+import { Index } from '../components/index';
+import { Home } from '../components/Home';
+import { EducationalServices } from '../components/educationalservices';
+import { ComingSoon } from '../components/comingsoon';
+import { OtherActivities } from '../components/otheractivities';
+import { ContactUs } from '../components/contactus';
+import { PageNotFound } from '../components/pageNotFound';
+// // import { Contact } from '../components/sections/contact';
+// import { PageNotFound } from '../components/sections/pageNotFound';
+// import { Unauthorized } from '../components/sections/Unauthorised';
+// import { Login } from '../components/authentication/login';
+// import { SignUp } from '../components/sections/signUp';
+// import { Profile } from '../components/sections/profile';
+// import { Dashboard } from '../components/sections/dashboard';
+
+function AppRoutes() {
+	return (
+		<Routes>
+			<Route path="/" element={<Index />}>
+				{/* This makes Home the default route */}
+				<Route index element={<Home />} />
+
+				{/* every other routes are from Outlet in Index */}
+				{/* Protected routes (auth + match check) */}
+				{/* <Route element={<ProtectedRoute requireMatch />}>
+					<Route path="scramble-questions/:id" element={<ScrambleQuestionsComponent />} />
+					<Route path="dashboard/:id/scramble-questions/:scrambleID" element={<ScrambleQuestionsComponent />} />
+					<Route path="profile/:id/contribute-questions" element={<ContributeQuestionsComponent />} />
+					<Route path="profile/:id" element={<Profile />} />
+					<Route path="dashboard/:id" element={<Dashboard />} />
+				</Route> */}
+
+				{/* Protected routes (auth only) */}
+				{/* <Route element={<ProtectedRoute />}>
+					<Route path="profile" element={<Profile />} />
+				</Route> */}
+
+				{/* Public routes (login and sign up) */}
+				<Route element={<PublicRoute />}>
+					<Route path="who-we-are" element={<EducationalServices />} />
+					<Route path="what-we-do" element={<ComingSoon />} />
+					<Route path="other-activities" element={<OtherActivities />} />
+					<Route path="contact-us" element={<ContactUs />} />
+					<Route path="*" element={<PageNotFound />} />
+				</Route>
+			</Route>
+		</Routes>
+	);
+}
+
+export {AppRoutes};

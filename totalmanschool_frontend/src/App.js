ebd830d6aadffd1af547bb3e7253a042abac2abb
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2026-04-14 19:37:28 +0100

    App.js:

---
+++ totalmanschool_frontend/src/App.js
@@ -0,0 +1,17 @@
+import { AppRoutes } from './routes/routes';
+import './assets/css/bootstrap.min.css'
+import './App.css';
+import './assets/css/mainStyles.css'
+import './assets/css/responsive.css'
+import './assets/css/fontawesome.css'
+import './assets/css/owl.css'
+
+function App() {
+  return (
+    <>
+        <AppRoutes />
+    </>
+  );
+}
+
+export default App;

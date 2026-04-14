99b27af66770c0858b30db4934c4034d3a8bad3f
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2026-04-14 19:37:26 +0100

    index.js:

---
+++ totalmanschool_frontend/src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { DeviceProvider } from './context/deviceTypeContext';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const isGitHubPages = window.location.hostname.includes('github.io');
+const RouterToUse = isGitHubPages ? HashRouter : BrowserRouter;
+root.render(
+  // <React.StrictMode>
+    <DeviceProvider>
+      <RouterToUse>
+        <App />
+      </RouterToUse>
+    </DeviceProvider>
+  // </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();

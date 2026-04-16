210f341bc8c11f78f205d29b06990db17f8f1bf7
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2026-04-16 02:13:15 +0100

    index.js: minor fix

--- totalmanschool_frontend/src/index.js
+++ totalmanschool_frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+// import "./icons";
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, HashRouter } from 'react-router-dom';
 import { DeviceProvider } from './context/deviceTypeContext';

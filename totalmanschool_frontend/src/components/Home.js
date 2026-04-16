b75c732eb6546119e2a41365da8e0c2887cda7ae
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2026-04-16 02:09:56 +0100

    Home.js: removed some stacked pages

--- totalmanschool_frontend/src/components/Home.js
+++ totalmanschool_frontend/src/components/Home.js
@@ -1,20 +1,15 @@
 import { Banner } from "./banner";
 import { Features } from "./fetaures";
-import { EducationalServices } from "./educationalservices";
+import { WhyChooseUs } from "./whychooseus";
 import { ComingSoon } from "./comingsoon";
-import { OtherActivities } from "./otheractivities";
-import { ContactUs } from "./contactus";
 
 function Home() {
 	return (
 		<>
 			<Banner />
 			<Features />
-			<EducationalServices />
+			<WhyChooseUs />
 			<ComingSoon />
-			<OtherActivities />
-			<ContactUs />
-			{/* <p>from home</p> */}
 		</>
 	)
 }

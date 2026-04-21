7a88be62f3c92489a6eab0e35e5f551465208be1
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2026-04-21 09:36:57 +0100

    feats.js: added animations

--- totalmanschool_frontend/src/feats/feats.js
+++ totalmanschool_frontend/src/feats/feats.js
@@ -4,7 +4,7 @@ function HangingHeader({headerText}) {
 	return (
 		<div className="col-md-12">
 			<div className="section-heading">
-				<h2>{headerText}</h2>
+				<h2 className="animate slide-from-top">{headerText}</h2>
 			</div>
 		</div>
 	)

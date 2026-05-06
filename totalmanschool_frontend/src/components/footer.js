4ea6a53cff17a92ab1071136daff616f5e22f5ec
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2026-05-06 09:40:41 +0100

    footer.js: fixed logos and links to each

--- totalmanschool_frontend/src/components/footer.js
+++ totalmanschool_frontend/src/components/footer.js
@@ -4,6 +4,25 @@ import { schoolInfo } from '../entry/entry';
 import { DafetiteFooter } from '../hooks/dafetiteFooter/dafetiteFooter';
 import { titleCase } from '../hooks/changeCase';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Fragment } from 'react';
+
+const contactIcons = [
+	{
+		icon: "location-dot",
+		item: schoolInfo.address,
+		link: null,
+	},
+	{
+		icon: "phone",
+		item: schoolInfo.mobile,
+		link: `tel:${schoolInfo.mobile}`,
+	},
+	{
+		icon: "envelope",
+		item: schoolInfo.email,
+		link: `mailto:${schoolInfo.email}`,
+	},
+]
 
 function Footer() {
 	return (
@@ -11,7 +30,28 @@ function Footer() {
 			<div className="Footer container">
 				<div className="row justify-content-center">
 					<div className="col-md-12 pt-4">
-						<p>
+						{contactIcons.map((item, idx) => {
+							return (
+								<Fragment key={item.item+idx}>
+									{item.link?
+									<>
+										<a
+										href={item.link} aria-label={item.item}>
+											<FontAwesomeIcon icon={item.icon} />
+											{item.item}
+										</a>
+										<br/>
+									</>
+									:
+									<p>
+										<FontAwesomeIcon icon={item.icon} />
+										{item.item}
+									</p>}
+									
+								</Fragment>
+							)
+						})}
+						{/* <p>
 							<FontAwesomeIcon icon="location-dot" />
 							{schoolInfo.address}</p>
 						<p>
@@ -19,7 +59,7 @@ function Footer() {
 							{schoolInfo.mobile}</p>
 						<p>
 							<FontAwesomeIcon icon="envelope" />
-							{schoolInfo.email}</p>
+							{schoolInfo.email}</p> */}
 					</div>
 					<div className="col-md-12">
 						<DafetiteFooter cpText={titleCase(`${schoolInfo.the} ${schoolInfo.totalman} ${schoolInfo.school}`)} />

8f1d9a21fc999e2194b113670b5f750a4579fd69
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2026-05-06 09:42:16 +0100

    feats.js: fixed component to handle more kwargs

--- totalmanschool_frontend/src/feats/feats.js
+++ totalmanschool_frontend/src/feats/feats.js
@@ -1,10 +1,12 @@
 
 
-function HangingHeader({headerText}) {
+function HangingHeader({headerText, invertColor=false, reduceMarginLine=false}) {
 	return (
 		<div className="col-md-12">
 			<div className="section-heading">
-				<h2 className="animate slide-from-top">{headerText}</h2>
+				<h2 className={`animate slide-from-top
+					${invertColor?'color-w border-w invert':''}
+					${reduceMarginLine?'why-choose-us':''}`}>{headerText}</h2>
 			</div>
 		</div>
 	)
